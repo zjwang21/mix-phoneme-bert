2f316ef11da4c6c52df4be0c5098474f31311732
zjwang21 <wzhijun21@gmail.com>
2023-04-18 18:58:57 +0800

    accuracy log

--- fairseq/fairseq/criterions/mix_phoneme_masked_lm.py
+++ fairseq/fairseq/criterions/mix_phoneme_masked_lm.py
@@ -68,16 +68,27 @@ class MixPhonemeMaskedLmLoss(FairseqCriterion):
             targets_p = targets['p_tgt_tokens'][masked_tokens_p]
 
         logits = model(**sample["net_input"], masked_tokens=masked_tokens_sp, targets_sp=targets_sp)[0]
+        logits_p = logits['phoneme'].view(-1, logits['phoneme'].size(-1))
+        logits_sp = logits['sup-phoneme'].view(-1, logits['sup-phoneme'].size(-1))
+
+        targets_p = targets_p.view(-1)
+        targets_sp = logits['targets_sp'].view(-1)
+
+        sample_size_sp = targets_sp.size(0)
+        phonemes = torch.argmax(torch.softmax(logits_p, dim=-1), dim=-1)
+        sup_phonemes = torch.argmax(torch.softmax(logits_sp, dim=-1), dim=-1)
+        acc_p = (phonemes == targets_p).sum()
+        acc_sp = (sup_phonemes == targets_sp).sum()
 
         loss_p = modules.cross_entropy(
-            logits['phoneme'].view(-1, logits['phoneme'].size(-1)),
-            targets_p.view(-1),
+            logits_p,
+            targets_p,
             reduction="sum",
             ignore_index=self.padding_idx,
         )
         loss_sp = modules.cross_entropy(
-            logits['sup-phoneme'],
-            logits['targets_sp'],
+            logits_sp,
+            targets_sp,
             reduction="sum",
             ignore_index=self.padding_idx,
         )
@@ -86,9 +97,12 @@ class MixPhonemeMaskedLmLoss(FairseqCriterion):
             "loss": loss if self.tpu else loss.data,
             "loss_p": loss_p if self.tpu else loss_p.data,
             "loss_sp": loss_sp if self.tpu else loss_sp.data,
+            "acc_p": acc_p,
+            "acc_sp": acc_sp,
             "ntokens": sample["ntokens"],
             "nsentences": sample["nsentences"],
             "sample_size": sample_size,
+            "sample_size_sp": sample_size_sp,
         }
         return loss, sample_size, logging_output
 
@@ -98,7 +112,10 @@ class MixPhonemeMaskedLmLoss(FairseqCriterion):
         loss_sum = sum(log.get("loss", 0) for log in logging_outputs)
         loss_sum_p = sum(log.get("loss_p", 0) for log in logging_outputs)
         loss_sum_sp = sum(log.get("loss_sp", 0) for log in logging_outputs)
+        acc_sum_p = sum(log.get("acc_p", 0) for log in logging_outputs)
+        acc_sum_sp = sum(log.get("acc_sp", 0) for log in logging_outputs)
         sample_size = sum(log.get("sample_size", 0) for log in logging_outputs)
+        sample_size_sp = sum(log.get("sample_size_sp", 0) for log in logging_outputs)
 
         metrics.log_scalar(
             "loss", loss_sum / sample_size / math.log(2), sample_size, round=3
@@ -108,6 +125,12 @@ class MixPhonemeMaskedLmLoss(FairseqCriterion):
         )
         metrics.log_scalar(
             "loss_sp", loss_sum_sp / sample_size / math.log(2), sample_size, round=3
+        )
+        metrics.log_scalar(
+            "acc_phoneme", acc_sum_p / sample_size / math.log(2), sample_size, round=3
+        )
+        metrics.log_scalar(
+            "acc_sup_phoneme", acc_sum_sp / sample_size_sp / math.log(2), sample_size_sp, round=3
         )
         metrics.log_derived(
             "ppl", lambda meters: utils.get_perplexity(meters["loss"].avg)
